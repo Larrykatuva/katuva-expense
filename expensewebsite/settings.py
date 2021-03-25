891214c69b5a5634a1542b08b6af22c5830b0ae6
Larrykatuva <>
2021-03-25 22:24:19 +0300

    Static files

--- expensewebsite/settings.py
+++ expensewebsite/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'expensewebsite/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
